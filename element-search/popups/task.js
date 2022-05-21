62a35e93b4138c0e520c5a74cde92ee01fc637a5
Екатерина Воробьёва <polskaya007@mail.ru>
2022-05-21 21:57:02 +0400

    v/3

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,32 +1,30 @@
-const popup = document.getElementById('modal_main');
-window.onload = function (){
-	popup.classList.add('modal_active');
-	console.log(popup.classList);
+const popup = document.getElementById("modal_main");
+//let popupName = popup.className;
+window.onload = function () {
+  popup.classList.add("modal_active");
+  console.log(popup.classList);
 };
 
-const closeButtons = Array.from(document.querySelectorAll('.modal__close'));
-for(let i = 0; i < closeButtons.length; i++){
-	closeButtons[i].onclick = function(){
-		if(closeButtons[i].closest('.modal')){
-		popup.classList.remove('modal_active');
-		}
-	}
+const closeButtons = Array.from(document.querySelectorAll(".modal__close"));
+for (let i = 0; i < closeButtons.length; i++) {
+  closeButtons[i].onclick = function () {
+    if (closeButtons[i].closest(".modal")) {
+      popup.classList.remove("modal_active");
+    }
+  };
 }
 
-const success = document.querySelector('.show-success');
-const showSuccess = document.getElementById('modal_success');
-const closeButtonModal = showSuccess.querySelector('.modal__content');
+const success = document.querySelector(".show-success");
+const showSuccess = document.getElementById("modal_success");
+const closeButtonModal = showSuccess.querySelector(".modal__content");
 
-success.onclick = function(){
-	showSuccess.classList.add('modal_active');
-	success.onclick = null;
-}
-	closeButtonModal.onclick = function (){
-		if(closeButtonModal.closest('.modal')){
-			showSuccess.classList.remove('modal_active');
+success.onclick = function () {
+  popup.classList.remove("modal_active");
+  showSuccess.classList.add("modal_active");
 
-		}
-
-	}
-
-	
+  closeButtonModal.onclick = function () {
+    if (closeButtonModal.closest(".modal")) {
+      showSuccess.classList.remove("modal_active");
+    }
+  };
+};
