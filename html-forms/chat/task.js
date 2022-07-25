4f5be30fb547fbec425dac7882c8c7fe5f93a741
Екатерина Воробьёва <polskaya007@mail.ru>
2022-07-26 01:47:32 +0400

    version 1.2

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -9,22 +9,31 @@ widget.addEventListener('click', () => {
 function enterFunc() {
 	const messages = document.getElementById('chat-widget__messages');
 
+
+
+
      messages.innerHTML  +=                 
 	        ` <div class="message message_client">
                 <div class="message__time"></div>
                 <div class="message__text"></div>
              </div> `
-     let messageTime = document.querySelector('.message__time');
 
-     let date = new Date();
-     let dateHours = date.getHours();
-     let dateMinutes = date.getMinutes();
-     messageTime.textContent = `${dateHours} : ${dateMinutes}`;
-     console.log(messageTime.textContent)
+
+
+
+   let messageTime = document.querySelector('.message__time');
+        let date = new Date();
+        let dateHours = date.getHours();
+        let dateMinutes = date.getMinutes();
+        messageTime.textContent = `${dateHours} : ${dateMinutes}`;
+        console.log(messageTime.textContent);
     
-	 
-     let messageText = document.querySelector('.message__text');
-     messageText = chatWidgetInput.value;
+    
+   let messageText = document.querySelector('.message__text');
+     messageText.textContent = chatWidgetInput.value;
+
+   e.target.reset();
+
     };
 
 
@@ -33,14 +42,18 @@ function enterFunc() {
 
 
  chatWidgetInput.addEventListener('keydown', function(e) {
+
     if(e.keyCode === 13){
+      //e.preventDefault();
         enterFunc();
+
+
     };
-
-
      if(!chatWidgetInput.checkValidity()){
         alert('Напишите нам что-нибудь!');
      }
 
-//chatWidgetInput.value.reset();
+
+
+//chatWidgetInput.value = '';
 })
